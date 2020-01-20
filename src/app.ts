f822c657bce93adac6fd8b89f294bab3158136da
Mgrdich <mgotm13@gmail.com>
2020-01-20 09:40:51 +0400

    small modifications

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 
 app.use(function (req: Request, res: Response, next: NextFunction) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization,Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -2,13 +2,38 @@ import {NextFunction, Request, Response} from "express";
 
 
 function getPosts(req: Request, res: Response, next: NextFunction) {
-    res.status(200).json({id: 1, name: "boxos"});
+    res.status(200).json({
+        posts: [
+            {
+                _id: '1',
+                title: "First Post",
+                content: "this is the first post",
+                imageUrl: "images/mgo.JPG",
+                creator: {
+                    name: "Mgo"
+                },
+                createdAt: new Date()
+
+            }
+        ]
+    });
 }
 
 function createPost(req: Request, res: Response, next: NextFunction) {
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({id: new Date().toISOString(), title, content});
+    res.status(201).json({
+        post: {
+            _id: new Date().toISOString(),
+            title,
+            content,
+            creator: {
+                name: "Mgo"
+            },
+            createdAt: new Date()
+        },
+        message: "Created Successfully"
+    });
 }
 
 export {getPosts, createPost};
